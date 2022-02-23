a0a3b6b0a2357c95e6b360b249e848a566c7f8b9
trushpatel <68613515+trushpatel@users.noreply.github.com>
2022-02-23 16:29:19 -0500

    Add files via upload

--- profile.py
+++ profile.py
@@ -1,42 +1,33 @@
+  
 import geni.portal as portal
-import geni.rspec.pg as rspec
+import geni.rspec.pg as pg
 import geni.rspec.igext as IG
-from lxml import etree as ET
 
-prefixForIP = "192.168.1."
-currentIP = 1
+pc = portal.Context()
+request = pc.makeRequestRSpec()
 
-# Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
-link = request.LAN("lan")
+tourDescription = \
+"""
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
+"""
 
-# Create a XenVM
-node = request.XenVM("node")
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+node = request.XenVM("docker")
+node.cores = 8
+node.ram = 8192
+node.routable_control_ip = "true" 
+
+bs_landing = node.Blockstore("bs_image", "/image")
+bs_landing.size = "500GB"
+  
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
-iface = node.addInterface("if" + str(currentIP))
-iface.component_id = "eth1"
-iface.addAddress(rspec.IPv4Address(prefixForIP + str(currentIP), "255.255.255.0"))
-currentIP = currentIP + 1
-link.addInterface(iface)
-
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
-
-# setup database
-node = request.XenVM("database")
-node.cores = 2
-node.ram = 2048
-node.routable_control_ip = "true" 
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-iface = node.addInterface("if" + str(currentIP))
-iface.component_id = "eth1"
-iface.addAddress(rspec.IPv4Address(prefixForIP + str(currentIP), "255.255.255.0"))
-link.addInterface(iface)
-node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_db.sh"))
-currentIP = currentIP + 1
-
-
-# Print the RSpec to the enclosing page.
-portal.context.printRequestRSpec()
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+  
+pc.printRequestRSpec(request)
